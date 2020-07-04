465d03131a31c6c3da56b7bbcb72d27f8794ad23
Ethan Netz <Ethannetz@hotmail.com>
2020-07-03 22:07:56 -0400

    utility now chekcs for propkeys

--- src/TilesCollectionUtlities/functions.ts
+++ src/TilesCollectionUtlities/functions.ts
@@ -1,6 +1,6 @@
 import { PropertyGroupKeys } from '../TilesCollection/interfaces'
 import { PropertyGroupValuesWithState, PropertyGroupValuesWithDidChange } from './interfaces'
-import {State} from '../TilesCollection/enums'
+import { State } from '../TilesCollection/enums'
 import powerbi from "powerbi-visuals-api";
 import { VisualSettings } from '../settings';
 
@@ -8,23 +8,23 @@ import { VisualSettings } from '../settings';
 export function getPropertyStateNameArr(propKeys: string[]): PropertyGroupKeys[] {
     let propertyGroupKeysArr: PropertyGroupKeys[] = []
     for (let i = 0; i < propKeys.length; i++)
-        if (propKeys[i].endsWith('A')) 
-        propertyGroupKeysArr.push(getPropertyStateNames(propKeys[i].slice(0, -1)))
+        if (propKeys[i].endsWith('A'))
+            propertyGroupKeysArr.push(getPropertyStateNames(propKeys, propKeys[i].slice(0, -1)))
     return propertyGroupKeysArr
 }
 
-export function getPropertyStateNames(propBase: string): PropertyGroupKeys{
+export function getPropertyStateNames(propKeys: string[], propBase: string): PropertyGroupKeys {
     return {
-            all: propBase+"A",
-            selected: propBase+"S",
-            unselected: propBase+"U",
-            hover: propBase+"H",
-            default: propBase+ "D"
-        }
+        get all(): string { return propKeys.indexOf(propBase + "A") > -1 ? propBase + "A" : null },
+        get selected(): string { return propKeys.indexOf(propBase + "S") > -1 ? propBase + "S" : null },
+        get unselected(): string { return propKeys.indexOf(propBase + "U") > -1 ? propBase + "U" : null },
+        get hover(): string { return propKeys.indexOf(propBase + "H") > -1 ? propBase + "H" : null },
+        get default(): string { return propKeys.indexOf(propBase + "D") > -1 ? propBase + "D" : null },
+    }
 }
 
 
-export function getObjectsToPersist(visualSettings: VisualSettings): powerbi.VisualObjectInstancesToPersist{
+export function getObjectsToPersist(visualSettings: VisualSettings): powerbi.VisualObjectInstancesToPersist {
     let objKeys = Object.keys(visualSettings)
     let objects: powerbi.VisualObjectInstancesToPersist = {
         merge: []
@@ -41,22 +41,21 @@ export function getObjectsToPersist(visualSettings: VisualSettings): powerbi.Vis
         }
 
         for (let j = 0; j < groupedKeyNamesArr.length; j++) {
-            let propertyGroupKeys: PropertyGroupKeys = groupedKeyNamesArr[j]
-            let type = typeof visualSettings[objKey][propertyGroupKeys.all]
-            let propertyState: PropertyGroupValuesWithState = {
-                all: visualSettings[objKey][propertyGroupKeys.all],
-                selected: visualSettings[objKey][propertyGroupKeys.selected],
-                unselected: visualSettings[objKey][propertyGroupKeys.unselected],
-                hover: visualSettings[objKey][propertyGroupKeys.hover],
+            let propKeys: PropertyGroupKeys = groupedKeyNamesArr[j]
+            let propValues: PropertyGroupValuesWithState = {
+                all: visualSettings[objKey][propKeys.all],
+                selected: visualSettings[objKey][propKeys.selected],
+                unselected: visualSettings[objKey][propKeys.unselected],
+                hover: visualSettings[objKey][propKeys.hover],
                 state: visualSettings[objKey].state,
-                default: visualSettings[objKey][propertyGroupKeys.default]
+                default: visualSettings[objKey][propKeys.default]
             }
-            let leveledPropertyState = levelProperties(propertyState)
+            let leveledPropertyState = levelProperties(propValues, propKeys)
             if (leveledPropertyState.didChange) {
-                object.properties[propertyGroupKeys.all] = leveledPropertyState.all
-                object.properties[propertyGroupKeys.selected] = leveledPropertyState.selected
-                object.properties[propertyGroupKeys.unselected] = leveledPropertyState.unselected
-                object.properties[propertyGroupKeys.hover] = leveledPropertyState.hover
+                object.properties[propKeys.all] = leveledPropertyState.all
+                object.properties[propKeys.selected] = leveledPropertyState.selected
+                object.properties[propKeys.unselected] = leveledPropertyState.unselected
+                object.properties[propKeys.hover] = leveledPropertyState.hover
             }
         }
         if (Object.keys(object.properties).length != 0)
@@ -65,46 +64,55 @@ export function getObjectsToPersist(visualSettings: VisualSettings): powerbi.Vis
     return objects
 }
 
-export function levelProperties(propertyStates: PropertyGroupValuesWithState): PropertyGroupValuesWithDidChange {
-    let _all = propertyStates.all
-    let _selected = propertyStates.selected
-    let _unselected = propertyStates.unselected
-    let _hover = propertyStates.hover
+export function levelProperties(propValues: PropertyGroupValuesWithState, propKeys: PropertyGroupKeys): PropertyGroupValuesWithDidChange {
+    let _all = propValues.all
+    let _selected = propValues.selected
+    let _unselected = propValues.unselected
+    let _hover = propValues.hover
     let allExists = false
     let nullValue = null
-    if(typeof _all == 'number'){
+    if (typeof _all == 'number') {
         allExists = _all >= 0
     } else {
         allExists = _all != null && _all.length > 0
         nullValue = ""
     }
 
-    if(_selected == null || _unselected == null || _hover == null){
-        _selected = _unselected = _hover = propertyStates.default
-    }
+    let overrideWithAll: boolean = propValues.state == State.all && allExists
 
-    if (propertyStates.state == State.all && allExists)
-        _selected = _unselected = _hover = _all
+    if (_selected == null && propKeys.selected)
+        _selected = overrideWithAll ? _all : propValues.default
+    if (_unselected == null && propKeys.unselected)
+        _unselected = overrideWithAll ? _all : propValues.default
+    if (_hover == null && propKeys.hover)
+        _hover = overrideWithAll ? _all : propValues.default
+
     if (_selected == _unselected && _selected == _hover)
         _all = _selected
+
     if (_selected != _unselected || _selected != _hover)
         _all = nullValue
+
+    let didChange: boolean = (propValues.all != _all ||
+        propValues.selected != _selected ||
+        propValues.unselected != _unselected ||
+        propValues.hover != _hover)
+
+
+
     return {
         all: _all,
         selected: _selected,
         unselected: _unselected,
         hover: _hover,
-        default: propertyStates.default,
-        didChange: !((propertyStates.all == _all) && 
-                    propertyStates.selected == _selected  && 
-                    propertyStates.unselected == _unselected &&
-                    propertyStates.hover == _hover)
+        default: propValues.default,
+        didChange: didChange
     }
 }
 
 
-export function getCorrectPropertyStateName(state: State, propBase: string): string{
-    switch(state){
+export function getCorrectPropertyStateName(state: State, propBase: string): string {
+    switch (state) {
         case State.all:
             return propBase + "A"
         case State.selected:
